ba05e659830ed76478d8e823dcb340f7ef416944
Artem Vasilev <vartem.box@gmail.com>
2020-08-08 02:48:45 +0300

    Finished eps-NFA -> NFA construction, 0 sorries

--- src/automata/epsnfa.lean
+++ src/automata/epsnfa.lean
@@ -24,6 +24,8 @@ inductive go (enfa : epsNFA S Q) : Q → list S → Q → Prop
 @[simp] def epsnfa_accepts_word (enfa : epsNFA S Q) (w : list S) : Prop := 
     ∃ {t}, go enfa enfa.start w t ∧ t ∈ enfa.term
 
+def eps_reach (enfa : epsNFA S Q) (q : Q) (r : Q) := go enfa q [] r
+
 @[simp] def lang_of_epsnfa (enfa : epsNFA S Q) := {w | epsnfa_accepts_word enfa w}
 
 def epsnfa_lang (lang : set (list S)) : Prop := 
@@ -39,102 +41,92 @@ begin
     { apply go.eps h (ih hfc) },
 end
 
-lemma epsnfa_go_exists_trans {enfa : epsNFA S Q} {a c : Q} 
-    {left right concat : list S} {clr : left ++ right = concat}:
-    go enfa a concat c → (∃ {b : Q}, go enfa a left b ∧ go enfa b right c) :=
+lemma epsnfa_go_exists_cons {enfa : epsNFA S Q} {a d : Q} 
+    {head : S} {tail concat : list S} (clr : list.cons head tail = concat):
+    go enfa a concat d → (∃ {b c : Q}, eps_reach enfa a b ∧ c ∈ enfa.next b head ∧ go enfa c tail d) :=
 begin
     intros go_ac,
-    induction go_ac with q  head tail q n f h prev ih  tail q n f h prev ih generalizing left, {
-        rw list.append_eq_nil at clr,
+    induction go_ac with q  head tail q n f h prev ih  tail q n f h prev ih, {
+        contradiction,
+    }, {
+        rw list.cons.inj_eq at clr,
         rcases clr with ⟨rfl, rfl⟩,
-        use [q, go.finish, go.finish],
+        use [q, n, go.finish, h, prev],
     }, {
-        by_cases left_nil : (left = []), {
-            subst left_nil,
-            dsimp at clr,
-            subst clr,
-            specialize @ih [],
-            use [q, go.finish, go.step h prev],
-        }, {
-            cases left, contradiction,
-            rw [list.cons_append, list.cons.inj_eq] at clr,
-            rcases clr with ⟨rfl, rfl⟩,
-            rcases @ih left_tl rfl with ⟨b, goleft, goright⟩,
-            use [b, go.step h goleft, goright],
-        }
-    }, {
-        subst clr,
-        rcases @ih left rfl with ⟨b, goleft, goright⟩,
-        use [b, go.eps h goleft, goright],
+        rcases ih clr with ⟨b, c, eps_ab, next_bc, tail_cd⟩,
+        use [b, c, go.eps h eps_ab, next_bc, tail_cd],
     }
 end
 
-def eps_reach (enfa : epsNFA S Q) (q : Q) (r : Q) := go enfa q [] r
+lemma epsnfa_go_exists_trans {enfa : epsNFA S Q} {a c : Q} 
+    {left right concat : list S} (clr : left ++ right = concat):
+    go enfa a concat c → (∃ {b : Q}, go enfa a left b ∧ go enfa b right c) :=
+begin
+    intros go_ac,
+    induction left with head tail ih generalizing a concat, {
+        subst clr,
+        use [a, go.finish, go_ac],
+    }, {
+        subst clr,
+        rcases epsnfa_go_exists_cons rfl go_ac with ⟨b, c', eps_ab, next_bc', tail_cd⟩,
+        specialize @ih c' (tail ++ right) rfl,
+        rcases ih tail_cd with ⟨real_b, go_real_b_tail, go_real_b_right⟩,
+
+        use [real_b],
+        use [epsnfa_go_trans eps_ab (go.step next_bc' go_real_b_tail)],
+        use [go_real_b_right],
+    }
+end
 
 def epsnfa_to_nfa (enfa : epsNFA S Q) : nfa.NFA S Q := {
     start := enfa.start,
     term := {x | ∃ t, eps_reach enfa x t ∧ t ∈ enfa.term},
-    next := λ q c, {r | ∃ x y, eps_reach enfa q x ∧ y ∈ enfa.next x c ∧ eps_reach enfa y r}
+    next := λ q c, {y | ∃ x, eps_reach enfa q x ∧ y ∈ enfa.next x c}
 }
 
-lemma epsnfa_to_nfa_singleton (en : epsNFA S Q) {n : nfa.NFA S Q} (w : S) (s1 e1 : Q)
-    : (n = epsnfa_to_nfa en) → (go en s1 [w] e1 ↔ nfa.go n s1 [w] e1) :=
+lemma epsnfa_to_nfa_accepts_iff_accepts
+    (en : epsNFA S Q) {n : nfa.NFA S Q} (w : list S) (st : Q)
+    : n = epsnfa_to_nfa en → ((∃ t : Q, go en st w t ∧ t ∈ en.term) ↔ (∃ t : Q, nfa.go n st w t ∧ t ∈ n.term)) :=
 begin
     rintro enfa_nfa,
     split, {
-        intro go_enfa,
-        induction go_enfa, {
-            exact nfa.go.finish,
+        rintro ⟨en_t, en_go, en_tterm⟩,
+        induction w with head tail hyp generalizing st, {
+            use [st, nfa.go.finish],
+            rw enfa_nfa,
+            dsimp [epsnfa_to_nfa],
+            use [en_t, en_go, en_tterm], 
         }, {
-            rcases go_enfa_ih with ⟨_⟩, {
-                sorry,
-            }, {
-                sorry,
-            }
-        }, {
-            sorry,
+            replace en_go := epsnfa_go_exists_cons rfl en_go,
+            rcases en_go with ⟨b, c, eps_stb, next_bc, tail_cent⟩,
+            specialize hyp c tail_cent,
+            rcases hyp with ⟨t, t_go, t_term⟩,
+            use t,
+            split, { 
+                refine nfa.go.step _ t_go, 
+                rw enfa_nfa, 
+                dsimp [epsnfa_to_nfa],
+                use [b, eps_stb, next_bc],
+            },
+            exact t_term,
         }
     }, {
-        subst enfa_nfa,
-        -- dsimp [epsnfa_to_nfa] at *,
-        rintro (_ | ⟨head, tail, q, n, f, h, go_nil⟩),
-        rcases go_nil,
-        rcases h with ⟨x, y, eps_s1x, ynext, eps_yn⟩,
-        exact epsnfa_go_trans eps_s1x (go.step ynext eps_yn),
-    }
-end
-
-lemma epsnfa_to_nfa_goes_to     
-    (en : epsNFA S Q) {n : nfa.NFA S Q} (w : list S) (s1 e1 : Q)
-    : (n = epsnfa_to_nfa en) → (w ≠ []) → (go en s1 w e1 ↔ nfa.go n s1 w e1) :=
-begin
-    rintro enfa_nfa,
-    induction w with fst tail hyp generalizing s1, {
-        tauto!,
-    }, {
-        rcases tail with _ | ⟨snd, rest⟩, {
-            rintro hdnil,
-            clear hdnil hyp,
-            refine epsnfa_to_nfa_singleton _ _ _ _ enfa_nfa,
+        rintro ⟨n_t, n_go, n_term⟩,
+        rw enfa_nfa at n_term,
+        dsimp [epsnfa_to_nfa] at n_term,
+        rcases n_term with ⟨en_t, en_t_eps, en_t_term⟩,
+        induction w with head tail hyp generalizing st, {
+            cases n_go, 
+            use [en_t, en_t_eps, en_t_term],
         }, {
-            intro h, clear h,
-            split, {
-                intro go_enfa,
-                rcases go_enfa with ⟨_⟩ | ⟨_, _, _, nxt, _, hnxt, prev⟩ | ⟨_, _, nxt, _, hnxt, prev⟩, {
-                    specialize @hyp nxt (list.cons_ne_nil _ _),
-                    replace hyp := hyp.1 prev,
-                    refine @nfa.go.step _ _ _ _ _ _ nxt _ _ hyp,
-                    subst enfa_nfa,
-                    use [s1, nxt, go.finish, hnxt, go.finish],
-                }, {
-                    specialize @hyp nxt (list.cons_ne_nil _ _),
-                    sorry,
-                },
-            }, {
-                sorry,
-            }
-        }
-    }
+            cases n_go,
+            specialize hyp n_go_n n_go_a,
+            rcases hyp with ⟨t, t_go, t_term⟩,
+            rw [enfa_nfa] at n_go_h, 
+            rcases n_go_h with ⟨x, x_go, x_next⟩,
+            use [t, and.intro (epsnfa_go_trans x_go (go.step x_next t_go)) t_term],
+        }     
+    }    
 end
 
 theorem epsnfa_to_nfa_eq {L : set (list S)} : epsnfa_lang L → nfa.nfa_lang L :=
@@ -142,41 +134,7 @@ begin
     rintro ⟨Q, enf, rfl⟩,
     use [Q, epsnfa_to_nfa enf],
     ext x,
-    dsimp,
-    split, {
-        rintro ⟨t, tgo, tterm⟩,
-        by_cases (x = []), {
-            subst h,
-            use enf.start,
-            split,
-            use [nfa.go.finish],
-            use [t, tgo, tterm],
-        }, {
-            use t,
-            have tmp := epsnfa_to_nfa_goes_to enf x (epsnfa_to_nfa enf).start t rfl h,
-            rw ← tmp,
-            use [tgo, t, epsnfa.go.finish, tterm], 
-        }
-    }, {
-        rintro ⟨t, tgo, tterm⟩,
-        by_cases xnil : x = [], {
-            subst xnil,
-            cases tgo,
-            use tterm,
-        }, {
-            rcases tterm with ⟨endt, endt_epsreach, endt_term⟩,
-            use endt,
-            have tmp := epsnfa_to_nfa_goes_to enf x (epsnfa_to_nfa enf).start t rfl xnil,
-            rw ← tmp at tgo,
-            split, {
-                convert epsnfa_go_trans tgo endt_epsreach,
-                simp only [list.append_nil], 
-            }, {
-                exact endt_term,
-            }
-        }
-    }
+    convert epsnfa_to_nfa_accepts_iff_accepts enf x enf.start rfl,    
 end
-
 
 end epsnfa
